74fedefc8189c4728db393098ab9bb9dd6c04f2d
arsims <arsim.sejdiu@bik-air.com>
2023-09-15 00:17:24 +0200

    Thread Application

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,54 @@
+"use server"
+
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose"
+import Community from "../models/community.model";
+
+interface UpdateUserProps {
+    userId: string,
+    username: string,
+    name: string,
+    bio: string,
+    image: string,
+    path: string
+}
+
+export async function updateUser({ userId, username, name, bio, image, path }: UpdateUserProps): Promise<void> {
+    try {
+        connectToDB();
+        await User.findOneAndUpdate(
+            { id: userId },
+            {
+                username: username.toLowerCase(),
+                name,
+                bio,
+                image,
+                onboarded: true,
+            },
+            { upsert: true } // upsert "update" and "insert" -> is a database operation that will update an existing row if a specified value already exist in a table
+            // -> and insert a new row if a specified value doesn't exist
+        );
+
+        if (path === "/profile/edit") {
+            revalidatePath(path); // -> Allows you to revalidate data associated with specific path. This is useful for scenarios where you want 
+            // -> to update your cached data without waiting for a revalidation data to expire
+        }
+    } catch (error: any) {
+        throw new Error(`Failed to create/update user: ${error.message}`);
+    }
+}
+
+export async function fetchUser(userId: string) {
+    try {
+        connectToDB();
+        return await User
+            .findOne({ id: userId })
+            .populate({
+                path: "communities",
+                model: Community,
+            });
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user: ${error.message}`);
+    }
+}
